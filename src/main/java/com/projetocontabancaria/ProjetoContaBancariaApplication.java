12bdfdf3d3165bd553df1d2cd2d8a6a24d3bc725
Caio Gomes <caio.vinipb@gmail.com>
2023-06-12 10:10:41 -0300

    Config inicial do projeto

---
+++ src/main/java/com/projetocontabancaria/ProjetoContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.projetocontabancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoContaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetocontabancaria/ProjetoContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocontabancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
