960ddf5cc231b2a25cf9991dbf5da2f898683a87
Mendonca065 <mendoncalucas065@gmail.com>
2023-06-12 11:46:33 -0300

    Ajustando Classes

--- src/main/java/com/projetocontabancaria/domain/Pessoa.java
+++ src/main/java/com/projetocontabancaria/domain/Pessoa.java
@@ -1,5 +1,51 @@
 package com.projetocontabancaria.domain;
 
-public class Pessoa {
+import java.time.LocalDate;
 
+public class Pessoa {
+	
+	private Long idPessoa;
+	private String nome;
+	private String cpf;
+	private LocalDate dataNascimento;
+	
+	public Pessoa(Long idPessoa, String nome, String cpf, LocalDate dataNascimento) {
+		this.idPessoa = idPessoa;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public Long getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
 }
